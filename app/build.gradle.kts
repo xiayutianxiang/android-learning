f066e9cf6145bc14987737288219e984e6ccadc7
山杉 <1455030401@qq.com>
2026-05-19 17:25:42 +0800

    commit:claude 添加自定义view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
